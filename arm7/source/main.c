54a0f29b4437fca810bbf8fafc05821427fae2f0
Arisotura <thetotalworm@gmail.com>
2021-11-05 17:10:49 +0100

    make things a bit less derpy

--- arm7/source/main.c
+++ arm7/source/main.c
@@ -1,54 +1,22 @@
-/*---------------------------------------------------------------------------------
-
-	default ARM7 core
-
-		Copyright (C) 2005 - 2010
-		Michael Noland (joat)
-		Jason Rogers (dovoto)
-		Dave Murphy (WinterMute)
-
-	This software is provided 'as-is', without any express or implied
-	warranty.  In no event will the authors be held liable for any
-	damages arising from the use of this software.
-
-	Permission is granted to anyone to use this software for any
-	purpose, including commercial applications, and to alter it and
-	redistribute it freely, subject to the following restrictions:
-
-	1.	The origin of this software must not be misrepresented; you
-		must not claim that you wrote the original software. If you use
-		this software in a product, an acknowledgment in the product
-		documentation would be appreciated but is not required.
-
-	2.	Altered source versions must be plainly marked as such, and
-		must not be misrepresented as being the original software.
-
-	3.	This notice may not be removed or altered from any source
-		distribution.
-
----------------------------------------------------------------------------------*/
 #include <nds.h>
 #include <dswifi7.h>
 #include <maxmod7.h>
 
-//---------------------------------------------------------------------------------
-void VblankHandler(void) {
-//---------------------------------------------------------------------------------
+
+void VblankHandler(void) 
+{
 	Wifi_Update();
 }
 
-
-//---------------------------------------------------------------------------------
-void VcountHandler() {
-//---------------------------------------------------------------------------------
+void VcountHandler() 
+{
 	inputGetAndSend();
 }
 
 volatile bool exitflag = false;
 
-//---------------------------------------------------------------------------------
-void powerButtonCB() {
-//---------------------------------------------------------------------------------
+void powerButtonCB() 
+{
 	exitflag = true;
 }
 
@@ -64,9 +32,9 @@ void biosDump(void* dst, const void* src, u32 len)
 	}
 }
 
-//---------------------------------------------------------------------------------
-int main() {
-//---------------------------------------------------------------------------------
+
+int main() 
+{
 	// clear sound registers
 	dmaFillWords(0, (void*)0x04000400, 0x100);
 
